fdda5a3223bd9a512cc81c7a18786c6b902ff9bc
Jahmyire <jahmyire@gmail.com>
2025-03-19 13:15:04 -0400

    config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
-const nextConfig = {
-  reactStrictMode: true,
-  output: "export", // Required for GitHub Pages
-  assetPrefix: isProd ? "/edencreates.github.io/" : "", // Ensures assets load correctly
-  basePath: isProd ? "/edencreates.github.io" : "", // Ensures proper routing
-  images: {
-    unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
-  },
-  trailingSlash: true, // Ensures correct routing for GitHub Pages
-};
+const nextConfig = {};
 
 export default nextConfig;
